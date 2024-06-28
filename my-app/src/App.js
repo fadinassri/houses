b9e2ed22da51f2d3647fe1343593dc341f2d98eb
Fadi Nassri <fadinassri@gmail.com>
2024-06-28 10:10:43 -0300

    Update text style for input to be displayed in one row

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -149,112 +149,124 @@ class HouseCostCalculator extends Component {
 
     return (
       <div className="container" style={{ marginLeft: '5px' }}>
-        <div>
-          <label>House Price:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1', margin: 2  }}>House Price:</label>
           <input
             type="number"
             name="housePrice"
             value={housePrice}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>After how many years are you planning to sell this house:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1', margin: 2 }}>After how many years are you planning to sell this house:</label>
           <input
             type="number"
             name="holdingLength"
             value={holdingLength}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Selling Price:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1' , margin: 2 }}>Selling Price:</label>
           <input
             type="number"
             name="sellingPrice"
             value={sellingPrice}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Down Payment:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1', margin: 2  }}>Down Payment:</label>
           <input
             type="number"
             name="downPayment"
             value={downPayment}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Mortgage Rate (%):</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1', margin: 2  }}>Mortgage Rate (%):</label>
           <input
             type="number"
             name="mortgageRate"
             value={mortgageRate}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Mortgage Period (years):</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1', margin: 2  }}>Mortgage Period (years):</label>
           <input
             type="number"
             name="mortgagePeriod"
             value={mortgagePeriod}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Tax per Year (Percentage of buying Price %):</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1' , margin: 2 }}>Tax per Year (Percentage of buying Price %):</label>
           <input
             type="number"
             name="taxPerYearPercentagre"
             value={taxPerYearPercentagre}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Buying Cost:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1' , margin: 2 }}>Buying Cost:</label>
           <input
             type="number"
             name="buyingCost"
             value={buyingCost}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Selling Cost Percentage (% of Selling Price):</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1', margin: 2 }}>Selling Cost Percentage (% of Selling Price):</label>
           <input
             type="number"
             name="sellingCostPercentage"
             value={sellingCostPercentage}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Monthly Insurance:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1' , margin: 2 }}>Monthly Insurance:</label>
           <input
             type="number"
             name="monthlyInsurance"
             value={monthlyInsurance}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Utility per Month:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1' , margin: 2 }}>Utility per Month:</label>
           <input
             type="number"
             name="utilityPerMonth"
             value={utilityPerMonth}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
-        <div>
-          <label>Maintenance Fees per Month:</label><br />
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+          <label style={{ flex: '1', margin: 2  }}>Maintenance Fees per Month:</label>
           <input
             type="number"
             name="maintenanceFeesPerMonth"
             value={maintenanceFeesPerMonth}
             onChange={this.handleInputChange}
+            style={{ flex: '2' }}
           />
         </div>
         <div style={{ padding: '10px 20px' }}>
